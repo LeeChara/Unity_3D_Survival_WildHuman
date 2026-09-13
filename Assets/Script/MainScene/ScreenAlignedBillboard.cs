80ef8da538b3e05217079970ed381248d615d631
LeeChara <radiorarian@gmail.com>
2026-09-14 01:34:47 +0900

    refactor: 빌보드 스크립트 이름을 실제 기법에 맞게 정리
    - PlayerBillboard > AxisAlignedBillboard
    - Billboard > ScreenAlignedBillboard
    - PlayerBillboard를 몬스터에도 활용하기 위해 이름을 변경하면서,
    - 코드를 약간 수정

--- Assets/Script/MainScene/ScreenAlignedBillboard.cs
+++ Assets/Script/MainScene/ScreenAlignedBillboard.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 
-public class Billboard : MonoBehaviour
+public class ScreenAlignedBillboard : MonoBehaviour
 {
     private Transform mainCamera;
 
+    // 투영면 정렬 빌보드
+    // 몬스터 체력바에서 사용 중
     private void Awake()
     {
         mainCamera = Camera.main.transform;
