6e90bcd53479d6a8c9860bbfb7288f0eefb421ba
LeeChara <radiorarian@gmail.com>
2026-09-20 05:43:51 +0900

    fix: 가시꽃살모사의 공격 판정, Windup 지속시간 수정
    - 공격 판정을 0.5s -> 1.0s
    - Windup 지속시간을 0.5s -> 1.0s
    - 히트박스의 크기에 비해 Windup이 과도하게 짧기 때문에 수정함
    - 추가로 애니메이션이 더 잘 드러나는 효과도 기대해볼 수 있음

--- Assets/Script/MainScene/Monster/RoseviperData.cs
+++ Assets/Script/MainScene/Monster/RoseviperData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class RoseviperData : MonsterData
 {
     [Header("공격")]
-    public float attackHitDuration = 0.5f;
+    public float attackHitDuration = 1.0f;
 
     [Header("후퇴")]
     public float retreatSpeed = 6f;
